1e0795323fc3e0d91eb72262fcd73f7925f58a43
holma <holmanlui121@gmail.com>
2018-03-20 18:06:01 -0500

    Added Sale filter

--- app/controllers/clothings_controller.rb
+++ app/controllers/clothings_controller.rb
@@ -1,23 +1,32 @@
 class ClothingsController < ApplicationController
   def index
 
-      @clothing_all = if (params[:term] || params[:categoryDrops]) && params[:filter] == "Recent"
-                        Clothing.where('name LIKE ? AND category_id LIKE ?',
-                                       "%#{params[:term]}%", "%#{params[:categoryDrops]}%")
-                                .where(updated_at: (Time.now - 6.hours)..Time.now)
-                                .page params[:page]
+    @clothing_all = if (params[:term] || params[:categoryDrops]) && params[:filter] == "Recent"
+                      Clothing.where('name LIKE ? AND category_id LIKE ?',
+                                     "%#{params[:term]}%", "%#{params[:categoryDrops]}%")
+                          .where(updated_at: (Time.now - 6.hours)..Time.now)
+                          .page params[:page]
 
-                      elsif params[:filter] == 'Recent'
-                          Clothing.where(updated_at: (Time.now - 6.hours)..Time.now).page params[:page]
+                    elsif (params[:term] || params[:categoryDrops]) && params[:filter] == "Sale"
+                      Clothing.where('name LIKE ? AND category_id LIKE ?',
+                                     "%#{params[:term]}%", "%#{params[:categoryDrops]}%")
+                          .where('sale_id == 1')
+                          .page params[:page]
 
-                      elsif params[:term] || params[:categoryDrops]
-                        Clothing.where('name LIKE ? AND category_id LIKE ?',
-                                  "%#{params[:term]}%", "%#{params[:categoryDrops]}%").page params[:page]
-                      else
-                        Clothing.page params[:page]
-                      end
+                    elsif params[:filter] == 'Recent'
+                      Clothing.where(updated_at: (Time.now - 6.hours)..Time.now).page params[:page]
 
-      @category_all = Category.where('Id IN (1,2,3,4,6,8,9)')
+                    elsif params[:filter] == 'Sale'
+                      Clothing.where('sale_id == 1')
+
+                    elsif params[:term] || params[:categoryDrops]
+                      Clothing.where('name LIKE ? AND category_id LIKE ?',
+                                     "%#{params[:term]}%", "%#{params[:categoryDrops]}%").page params[:page]
+                    else
+                      Clothing.page params[:page]
+                    end
+
+    @category_all = Category.where('Id IN (1,2,3,4,6,8,9)')
   end
 
   def show
